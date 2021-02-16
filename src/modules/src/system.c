e6b2b49a614a5fc1953dccd6aa6a188e10e4de41
Aranyyan <flopp_pinglorna2@hotmail.se>
2021-02-16 12:50:03 +0100

    Commented out stabilizer again.

--- src/modules/src/system.c
+++ src/modules/src/system.c
@@ -197,15 +197,15 @@ void systemTask(void *arg)
   commanderInit(); // ???
   //DEBUG_PRINT("commander init\n");
 
-  StateEstimatorType estimator = anyEstimator;
+  //StateEstimatorType estimator = anyEstimator;
   estimatorKalmanTaskInit(); // Software, functional but not usable without sensors
-  //DEBUG_PRINT("Kalman estimator init\n"),
+  DEBUG_PRINT("Kalman estimator init\n"),
   deckInit(); // 1-wire...?
   DEBUG_PRINT("deck init\n");
-  estimator = deckGetRequiredEstimator();
-  DEBUG_PRINT("Got estimator...\n");
-  stabilizerInit(estimator);
-  DEBUG_PRINT("stabilizer init\n");
+  //estimator = deckGetRequiredEstimator();
+  //DEBUG_PRINT("Got estimator...\n");
+  //stabilizerInit(estimator);
+  //DEBUG_PRINT("stabilizer init\n");
   if (deckGetRequiredLowInterferenceRadioMode() && platformConfigPhysicalLayoutAntennasAreClose())
   {
     platformSetLowInterferenceRadioMode();
